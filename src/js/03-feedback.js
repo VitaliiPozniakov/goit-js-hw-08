b20ae7a4ebcb2381cad09341b2b14265c4308460
VitaliiPozniakov <vertalen89@gmail.com>
2022-06-03 16:00:19 +0300

    Update third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,6 @@
 import lodashThrottle from 'lodash.throttle';
 
-let data = { email: '', message: '' };
+let formData = { email: '', message: '' };
 
 const STORAGE_KEY = `feedback-form-state`;
 
@@ -20,14 +20,18 @@ function completeForm() {
   refs.form.addEventListener('input', lodashThrottle(onFormInput, 500));
 
   if (parsedData) {
-    refs.textarea.value = parsedData.message;
+    refs.textarea.value = parsedData.message;  
     refs.input.value = parsedData.email;
   }
 }
 
 function onFormInput(e) {
-  data = { email: refs.input.value, message: refs.textarea.value };
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+// console.log(e.target)
+// console.log(e.target.name)
+// console.log(e.target.value)
+formData[e.target.name] = e.target.value
+//   formData = { email: refs.input.value, message: refs.textarea.value };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onFormSubmit(e) {
